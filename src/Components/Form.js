eafb1c3b01939ea663bc911a35b7f5cbf6321c40
Cameron Aragon <caragon4695@gmail.com>
2021-06-21 10:15:02 -0600

    Update select inputs with correct values

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../styles/Form.css';
 
 function Form() {
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
     const [form, setState] = useState({
         impressions: '',
         clicks: '',
@@ -17,7 +18,6 @@ function Form() {
         });
     };
 
-    console.log(form.impressions)
     return (
         <form>
             <h2>Filter Data</h2>
@@ -26,45 +26,45 @@ function Form() {
                 Number of impressions: <br></br>
                     <select value={form.impressions} name='impressions' onChange={handleChange}>
                         <option value='' selected disbaled hidden>None</option>
-                        <option value='grapefruit'>Grapefruit</option>
-                        <option value='lime'>Lime</option>
-                        <option value='coconut'>Coconut</option>
-                        <option value='mango'>Mango</option>
+                        <option value='700+'>700+</option>
+                        <option value='400-699'>400-699</option>
+                        <option value='100-399'>100-399</option>
+                        <option value='0-99'>0-99</option>
                     </select>
                 </label>
                 <label>
                 Clicks per product: <br></br>
-                    <select name='clicks'>
+                    <select value={form.clicks} name='clicks' onChange={handleChange}>
                         <option value='' selected disbaled hidden>None</option>
-                        <option value='grapefruit'>Grapefruit</option>
-                        <option value='lime'>Lime</option>
-                        <option value='coconut'>Coconut</option>
-                        <option value='mango'>Mango</option>
+                        <option value='100'>100+</option>
+                        <option value='75-99'>75-99</option>
+                        <option value='25-74'>25-74</option>
+                        <option value='0-24'>0-24</option>
                     </select>
                 </label>
                 <label>
                 Platform: <br></br>
-                    <select name='platform'>
+                    <select value={form.platform} name='platform' onChange={handleChange}>
                         <option value='' selected disbaled hidden>None</option>
-                        <option value='grapefruit'>Grapefruit</option>
-                        <option value='lime'>Lime</option>
-                        <option value='coconut'>Coconut</option>
-                        <option value='mango'>Mango</option>
+                        <option value='Amazon'>Amazon</option>
+                        <option value='Facebook'>Facebook</option>
+                        <option value='Google'>Google</option>
+                        <option value='Twitter'>Twitter</option>
+                        <option value='LinkedIn'>LinkedIn</option>
                     </select>
                 </label>
                 <label>
                 Product: <br></br>
-                    <select name=''>
+                    <select value={form.product} name='product' onChange={handleChange}>
                         <option value='' selected disbaled hidden>None</option>
-                        <option value='grapefruit'>Grapefruit</option>
-                        <option value='lime'>Lime</option>
-                        <option value='coconut'>Coconut</option>
-                        <option value='mango'>Mango</option>
+                        {alphabet.map(letter => {
+                            return <option value={letter}>{letter}</option>
+                        })}
                     </select>
                 </label>
                 <label>
                 Date Range: <br></br>
-                    <select name=''>
+                    <select name='' onChange={handleChange}>
                         <option value='' selected disbaled hidden>None</option>
                         <option value='grapefruit'>Grapefruit</option>
                         <option value='lime'>Lime</option>
