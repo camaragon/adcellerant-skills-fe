50631064f15cdd86ba1b443964ecfbfc4a6c6c4f
Cameron Aragon <caragon4695@gmail.com>
2021-06-20 21:48:43 -0600

    Use table tags

--- src/Components/Ad.js
+++ src/Components/Ad.js
@@ -2,14 +2,22 @@ import '../styles/Ad.css'
 
 function Ad({id, product, date, platform, impressions, clicks}) {
     return (
-        <div className='adContainer'>
-            <p>{id}</p>
-            <p>{product}</p>
-            <p>{date}</p>
-            <p>{platform}</p>
-            <p>{impressions}</p>
-            <p>{clicks}</p>
-        </div>
+        <tr>
+            <td>{id}</td>
+            <td>{product}</td>
+            <td>{date}</td>
+            <td>{platform}</td>
+            <td>{impressions}</td>
+            <td>{clicks}</td>
+        </tr>
+        // <div className='adContainer'>
+        //     <p>{id}</p>
+        //     <p>{product}</p>
+        //     <p>{date}</p>
+        //     <p>{platform}</p>
+        //     <p>{impressions}</p>
+        //     <p>{clicks}</p>
+        // </div>
     )
 }
 
