cfe3533942c97a30d01af944ff4387f9c9d945bc
Cameron Aragon <caragon4695@gmail.com>
2021-06-20 21:26:18 -0600

    Display all data from the Ad component

--- src/Components/Ad.js
+++ src/Components/Ad.js
@@ -1,6 +1,15 @@
-function Ad({platform}) {
+import '../styles/Ad.css'
+
+function Ad({id, product, date, platform, impressions, clicks}) {
     return (
-        <p>{platform}</p>
+        <div className='adContainer'>
+            <p>{id}</p>
+            <p>{product}</p>
+            <p>{date}</p>
+            <p>{platform}</p>
+            <p>{impressions}</p>
+            <p>{clicks}</p>
+        </div>
     )
 }
 
