ebae4e06b974098527d437f3894073528c81dc0b
Cameron Aragon <caragon4695@gmail.com>
2021-06-21 17:03:46 -0600

    Build clicks filter functionality

--- src/Components/App.js
+++ src/Components/App.js
@@ -15,34 +15,57 @@ function App() {
   }, [])
 
   const updateData = (form) => {
-    filterImpressions
+    filterImpressions(form);
+
     // pass each filtered data set to each helper function until the 
     // resulting data can be set to state
   }
 
-  const filterImpressions = (data) => {
-    switch (data) {
+  const filterImpressions = (form) => {
+    switch (form.impressions) {
       case 0:
-        return state;
+        filterClicks(state, form);
         break;
       case 1:
         const filtered = state.filter(ad => ad.impressions >= 700);
-        return filtered;
+        filterClicks(filtered, form);
         break;
       case 2:
         const filtered = state.filter(ad => ad.impressions < 700 && ad.impressions >= 400);
-        return filtered;
+        filterClicks(filtered, form);
         break;
       case 3:
         const filtered = state.filter(ad => ad.impressions < 400 && ad.impressions >= 100);
-        return filtered;
+        filterClicks(filtered, form);
         break;
       case 4:
         const filtered = state.filter(ad => ad.impressions < 100 && ad.impressions >= 0);
-        return filtered;
-        break:
+        filterClicks(filtered, form);
+        break;
     }
+  }
 
+  const filterClicks = (data, form) => {
+    switch (form.clicks) {
+      case 0:
+        filterPlatform(data, form);
+        break;
+      case 1:
+        const filtered = data.filter(ad => ad.clicks >= 100);
+        filterPlatform(filtered, form);
+      case 2:
+        const filtered = state.filter(ad => ad.clicks < 100 && ad.clicks >= 75);
+        filterPlatform(filtered, form);
+        break;
+      case 3:
+        const filtered = state.filter(ad => ad.clicks < 75 && ad.clicks >= 25);
+        filterPlatform(filtered, form);
+        break;
+      case 4:
+        const filtered = state.filter(ad => ad.clicks < 25 && ad.clicks >= 0);
+        filterPlatform(filtered, form);
+        break;
+    }
   }
 
   return (
