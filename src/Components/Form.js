158f46c8e3d4923c0cd674442253980c1730ef8a
Cameron Aragon <caragon4695@gmail.com>
2021-06-20 18:36:22 -0600

    Create dropdown select input

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,18 @@
 function Form() {
     return (
-        <h2>Hello</h2>
+        <form>
+            <label>
+            Pick your favorite flavor: <br></br>
+                <select name=''>
+                    <option value='' selected disbaled hidden>None</option>
+                    <option value='grapefruit'>Grapefruit</option>
+                    <option value='lime'>Lime</option>
+                    <option value='coconut'>Coconut</option>
+                    <option value='mango'>Mango</option>
+                </select>
+            </label>
+        {/* <input type='submit' value='Submit' /> */}
+        </form>
     )
 }
 
