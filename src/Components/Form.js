1748349ccdf8e974cce7ff968e0bd1291f0adef4
Cameron Aragon <caragon4695@gmail.com>
2021-06-20 18:49:25 -0600

    Setup form with dummy inputs

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,17 +1,52 @@
+import '../styles/Form.css';
+
 function Form() {
     return (
         <form>
-            <label>
-            Pick your favorite flavor: <br></br>
-                <select name=''>
-                    <option value='' selected disbaled hidden>None</option>
-                    <option value='grapefruit'>Grapefruit</option>
-                    <option value='lime'>Lime</option>
-                    <option value='coconut'>Coconut</option>
-                    <option value='mango'>Mango</option>
-                </select>
-            </label>
-        {/* <input type='submit' value='Submit' /> */}
+            <h2>Filter Data</h2>
+            <div className='filterForm'>
+                <label>
+                Number of impressions: <br></br>
+                    <select name=''>
+                        <option value='' selected disbaled hidden>None</option>
+                        <option value='grapefruit'>Grapefruit</option>
+                        <option value='lime'>Lime</option>
+                        <option value='coconut'>Coconut</option>
+                        <option value='mango'>Mango</option>
+                    </select>
+                </label>
+                <label>
+                Clicks per product: <br></br>
+                    <select name=''>
+                        <option value='' selected disbaled hidden>None</option>
+                        <option value='grapefruit'>Grapefruit</option>
+                        <option value='lime'>Lime</option>
+                        <option value='coconut'>Coconut</option>
+                        <option value='mango'>Mango</option>
+                    </select>
+                </label>
+                <label>
+                Platform: <br></br>
+                    <select name=''>
+                        <option value='' selected disbaled hidden>None</option>
+                        <option value='grapefruit'>Grapefruit</option>
+                        <option value='lime'>Lime</option>
+                        <option value='coconut'>Coconut</option>
+                        <option value='mango'>Mango</option>
+                    </select>
+                </label>
+                <label>
+                Date Range: <br></br>
+                    <select name=''>
+                        <option value='' selected disbaled hidden>None</option>
+                        <option value='grapefruit'>Grapefruit</option>
+                        <option value='lime'>Lime</option>
+                        <option value='coconut'>Coconut</option>
+                        <option value='mango'>Mango</option>
+                    </select>
+                </label>
+            </div>
+        <input className='filterSubmit' type='submit' value='Submit' />
         </form>
     )
 }
