c72ed47c9bf28cfbdf31e6a2f3a465e34dc97cb9
Cameron Aragon <caragon4695@gmail.com>
2021-06-21 16:46:54 -0600

    Build impression filter conditional

--- src/Components/App.js
+++ src/Components/App.js
@@ -14,7 +14,34 @@ function App() {
     })
   }, [])
 
-  const filterData = () => {
+  const updateData = (form) => {
+    filterImpressions
+    // pass each filtered data set to each helper function until the 
+    // resulting data can be set to state
+  }
+
+  const filterImpressions = (data) => {
+    switch (data) {
+      case 0:
+        return state;
+        break;
+      case 1:
+        const filtered = state.filter(ad => ad.impressions >= 700);
+        return filtered;
+        break;
+      case 2:
+        const filtered = state.filter(ad => ad.impressions < 700 && ad.impressions >= 400);
+        return filtered;
+        break;
+      case 3:
+        const filtered = state.filter(ad => ad.impressions < 400 && ad.impressions >= 100);
+        return filtered;
+        break;
+      case 4:
+        const filtered = state.filter(ad => ad.impressions < 100 && ad.impressions >= 0);
+        return filtered;
+        break:
+    }
 
   }
 
