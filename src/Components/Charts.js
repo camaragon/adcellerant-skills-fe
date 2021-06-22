4a099988f19ffaac623774cfefe182e9a07fa400
Cameron Aragon <caragon4695@gmail.com>
2021-06-22 09:51:56 -0600

    Render a dummy pie chart

--- src/Components/Charts.js
+++ src/Components/Charts.js
@@ -1,8 +1,34 @@
 import '../styles/Charts.css';
+import {Pie} from 'react-chartjs-2';
 
 const Charts = () => {
     return (
-        <h3>Helllo</h3>
+        <div>
+        <Pie
+            data={{
+                labels: [1, 2, 3],
+                datasets: [{
+                    data: [2000, 4000, 2850],
+                    backgroundColor: ['red', 'blue', 'green']
+                }]
+            }}
+            height='600'
+            width='600'
+            options={{ responsive: false, maintainAspectRatio: false }}
+        />
+        <Pie
+            data={{
+                labels: [1, 2, 3],
+                datasets: [{
+                    data: [2000, 4000, 2850],
+                    backgroundColor: ['red', 'blue', 'green']
+                }]
+            }}
+            // height='600'
+            // width='600'
+            options={{ responsive: true, maintainAspectRatio: true }}
+        />
+        </div>
     )
 };
 
