816cb5ae4d1b0ee008bad233a01586d70fc1a2ce
Cameron Aragon <caragon4695@gmail.com>
2021-06-21 20:20:12 -0600

    Add functionality to reset filter

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,6 +6,7 @@ import getData from '../fetchRequests';
 
 function App() {
   const [state, setState] = useState([]);
+  const [filtered, setFiltered] = useState(false)
 
   useEffect(() => {
     getData()
@@ -16,67 +17,51 @@ function App() {
 
   const updateData = (form) => {
     const result = filterImpressions(form);
-    console.log('hello')
     setState(result);
-    // pass each filtered data set to each helper function until the 
-    // resulting data can be set to state
+    setFiltered(true);
   }
-
-  // const filterData = () => {
-  //   const filtered = filterImpressions();
-  //   fil
-  // }
 
   const filterImpressions = (form) => {
     if (form.impressions === '') {
-      filterClicks(state, form);
+      return filterClicks(state, form);
     } else if (form.impressions.includes('-')) {
       const range = form.impressions.split('-');
       const filtered = state.filter(ad => ad.impressions <= range[1] && ad.impressions >= range[0]);
-      filterClicks(filtered, form);
+      return filterClicks(filtered, form);
     } else {
       const filtered = state.filter(ad => ad.impressions >= form.impressions)
-      filterClicks(filtered, form);
+      return filterClicks(filtered, form);
     }
   }
 
-  // const something = () => {
-  //   if (form.clicks.includes('-')) {
-  //     const numbers = form.clicks.split('-');
-  //     const filtered = data.filter(ad.clicks <= numbers[1] && ad.clicks >= numbers[0]);
-  //   } else {
-  //     const filtered = data.filter(ad => ad.clicks >= form.clicks)
-  //   }
-  // }
-
   const filterClicks = (data, form) => {
     if (form.clicks === '') {
-      filterPlatform(data, form);
+      return filterPlatform(data, form);
     } else if (form.clicks.includes('-')) {
       const range = form.clicks.split('-');
       const filtered = data.filter(ad => ad.clicks <= range[1] && ad.clicks >= range[0]);
-      filterPlatform(filtered, form);
+      return filterPlatform(filtered, form);
     } else {
       const filtered = data.filter(ad => ad.clicks >= form.clicks)
-      filterPlatform(filtered, form);
+      return filterPlatform(filtered, form);
     }
   }
 
   const filterPlatform = (data, form) => {
     if (form.platform === '') {
-      filterProduct(data, form);
+      return filterProduct(data, form);
     } else {
       const filtered = data.filter(ad => ad.platform === form.platform);
-      filterProduct(filtered, form);
+      return filterProduct(filtered, form);
     }
   }
 
   const filterProduct = (data, form) => {
     if (form.product === '') {
-      filterDateRange(data, form);
+      return filterDateRange(data, form);
     } else {
       const filtered = data.filter(ad => ad.product === form.product);
-      filterDateRange(data, form);
+      return filterDateRange(filtered, form);
     }
   }
 
@@ -97,13 +82,26 @@ function App() {
     }
   }
 
+  const resetFilter = (event) => {
+    event.preventDefault();
+    getData()
+    .then(data => {
+      setState(data);
+    })
+    setFiltered(false);
+  }
+
   return (
     <>
       <header >
         <h1>Advertsing Analytics</h1>
         <p>{new Date().toLocaleString()}</p>
       </header>
-      <Form updateData={updateData}/>
+      {!filtered ? <Form updateData={updateData} /> : 
+      <div className='postFilter'>
+        <h3>{state.length} Results</h3>
+        <button className='filterReset' onClick={resetFilter}>Reset Filter</button>
+      </div>}
       <Analytics data={state}/>
     </>
   );
